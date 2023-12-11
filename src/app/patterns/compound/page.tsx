f16067e0fde85822ae3e4e9a8232e710909734dd
nakamura0907 <53244048+nakamura0907@users.noreply.github.com>
2023-12-12 00:13:25 +0900

    ディレクトリ作成

---
+++ src/app/patterns/compound/page.tsx
@@ -0,0 +1,7 @@
+const Page = () => {
+    return(
+        <a href="https://zenn.dev/morinokami/books/learning-patterns-1/viewer/compound-pattern">複合パターン</a>
+    )
+}
+
+export default Page;
---
+++ src/app/patterns/soc/page.tsx
@@ -0,0 +1,7 @@
+const Page = () => {
+    return(
+        <a href="https://azukiazusa.dev/blog/server-components-testing/">React Server Components のテスト手法</a>
+    )
+}
+
+export default Page;
